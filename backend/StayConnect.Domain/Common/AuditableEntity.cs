80c3d9ed46631573ff31f98048a9c91134c0adf2
johndoe101om <johndoeunique101@gmail.com>
2026-06-13 10:23:59 +0530

    Add domain base entity

---
+++ backend/StayConnect.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,13 @@
+namespace StayConnect.Domain.Common;
+
+public abstract class AuditableEntity
+{
+    public Guid Id { get; protected set; } = Guid.NewGuid();
+    public DateTime CreatedAtUtc { get; protected set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAtUtc { get; protected set; }
+
+    public void MarkUpdated()
+    {
+        UpdatedAtUtc = DateTime.UtcNow;
+    }
+}
